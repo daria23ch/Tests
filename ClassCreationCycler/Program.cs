2f5f1085ef89018500dba5da4e7cdc0396c34c07
Дарья Черкасова <chercasova_dasha@mail.ru>
2021-01-16 17:45:40 +0700

    fix

---
+++ ClassCreationCycler/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassCreationCycler
+{
+    class Cycler
+    {
+        private List<Object> cyclers = new List<Object>();
+
+        public Cycler(List<Object> lists)
+        {
+            this.cyclers = lists;
+        }
+
+        public void GetNext(int n)
+        {
+            
+            if (n<cyclers.Count && n>0)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine(cyclers[i]);
+                }
+            }
+            else
+            {
+                do
+                {
+                    foreach (var i in cyclers)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    n -= cyclers.Count;
+                } while (n >= cyclers.Count);
+            }
+
+        }
+        
+    }
+    class Program
+    {
+         
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Object> cyc = new List<Object>() { 1,2,4,5,6};
+            var cyclers = new Cycler(cyc);
+            cyclers.GetNext(n);
+            
+            
+        }
+    }
+    
+}
---
+++ CountNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char tempVariable='0';
+            int count= 0;
+            string stringer = Console.ReadLine();
+            string result = "";
+            char[] mass = stringer.ToCharArray();
+            char[] number = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+            for (int i = 0; i < number.Length; i++)
+            {
+                
+                for (int j = 0; j < mass.Length; j++)
+                {
+                    
+                    if (mass[j]==number[i])
+                    {
+                        count++;
+                        tempVariable = number[i];
+                    }
+                }
+                if (count != 0)
+                {
+                    result += tempVariable + ": " + count.ToString() + " | ";
+                }
+                count = 0;
+            }
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }    
+    }
+}
